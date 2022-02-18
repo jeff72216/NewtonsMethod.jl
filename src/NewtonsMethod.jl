e696bd7a4f152d3274e72e0ef81204b77ee38c1d
Jeff Hsieh <jeff_hsieh@sfu.ca>
2022-02-18 13:26:31 -0800

    creating new functions

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,42 @@
 module NewtonsMethod
 
-# Write your package code here.
+using LinearAlgebra, ForwardDiff
+
+function newtonroot(f, f′; x₀, tolerance=1E-7, maxiter=1000)
+    x_old = x₀
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - (f(x_old) / f′(x_old))
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    if iter > maxiter && normdiff > tolerance
+        return NaN
+    else
+        return x_old
+    end
+end
+
+function newtonroot(f; x₀, tolerance=1E-7, maxiter=1000)
+    x_old = x₀
+    f′ = x -> ForwardDiff.derivative(f, x)
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - (f(x_old) / f′(x_old))
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    if iter > maxiter && normdiff > tolerance
+        return NaN
+    else
+        return x_old
+    end
+end
+
+export newtonroot
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-    # Write your tests here.
+    f(x) = 5x^2 - 16x + 3
+    f′(x) = 10x - 16
+    g(x) = x^2 + 2
+    g′(x) = 2x
+    @test abs(newtonroot(f, f′, x₀=4) - 3) < 1e-7
+    @test abs(newtonroot(f, f′, x₀=0) - 0.2) < 1e-7
+    @test abs(newtonroot(f, x₀=4) - 3) < 1e-7
+    @test abs(newtonroot(f, x₀=0) - 0.2) < 1e-7
+    @test abs(newtonroot(f, f′, x₀=BigFloat(0.3)) - 0.2) < 1e-7
+    @test abs(newtonroot(f, x₀=BigFloat(0.3)) - 0.2) < 1e-7
+    @test isnan(newtonroot(g, g′, x₀=0))
+    @test isnan(newtonroot(f, f′, x₀=4, maxiter=1))
+    @test abs(newtonroot(f, f′, x₀=10, tolerance=1E+4) - 3) > 1e-1
 end
